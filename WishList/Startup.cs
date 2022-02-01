72ce7fe22241820dfb1b6cfb2e4a8378c0c8ba99
JavierFriasDev <j.frias.di@hotmail.com>
2022-02-01 18:20:42 -0500

    Create Home Views and the HomeController

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -18,9 +18,7 @@ namespace WishList
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
